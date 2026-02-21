ec84ca476d5d92ebe582661e92a241216390a948
praveen20-india <praveenkv21@gmail.com>
2026-02-21 23:27:39 +0100

    Clean initial commit

--- restore.sh
+++ restore.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# Create all missing directories
+mkdir -p data notebooks report/result_images results
+
+# Copy the dataset files
 cp ../full_dataset_mesh.stl data/
 cp ../half_dataset_mesh.stl data/
 cp ../third_dataset_mesh.stl data/
 cp ../quarter_dataset_mesh.stl data/
 
+# Run the generation scripts
 source venv/bin/activate
 python3 src/build_notebook.py
 python3 -m src.main --save-plots
